e23f917b008753724cf7b51f6d9986ba755641fa
oanap <oana.panduru@gmail.com>
2020-12-29 22:41:19 +0200

    char data type exercise

---
+++ characterExercises/src/characterExercises/MyChar.java
@@ -0,0 +1,70 @@
+package characterExercises;
+
+public class MyChar {
+	private char ch;
+
+	MyChar(char ch) {
+		this.ch = Character.toLowerCase(ch);
+	}
+
+	char getCharacter() {
+		return this.ch;
+	}
+
+	void print() {
+		System.out.println(this.ch);
+	}
+
+	boolean isVowel() {
+		if (this.ch == 'a' || this.ch == 'e' || this.ch == 'i' || this.ch == 'o' || this.ch == 'u') {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	boolean isDigit() {
+		if (this.ch >= 48 && this.ch <= 57) {
+			return true;
+		} else {
+			return false;
+		}
+			
+		}
+
+		boolean isConsonante() {
+			boolean testing = isVowel();
+			boolean testing2 = isAlphabet();
+			if (testing == true && testing2 == true) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+
+		boolean isAlphabet() {
+			if (this.ch >= 97 && this.ch <= 122) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		void alphabet() {
+			for (char i = 97; i <= 122; i++) {
+				System.out.println(i);
+			}
+		}
+
+		void alphabetUpper() {
+			for (char i = 65; i <= 90; i++) {
+				System.out.println(i);
+			}
+		}
+
+
+	}
+
+
---
+++ characterExercises/src/characterExercises/MyCharRunner.java
@@ -0,0 +1,16 @@
+package characterExercises;
+
+public class MyCharRunner {
+
+	public static void main(String[] args) {
+		MyChar character = new MyChar('c');
+		character.print();
+		System.out.println(character.isVowel());
+		System.out.println(character.isDigit());
+		System.out.println(character.isConsonante());
+		System.out.println(character.isAlphabet());
+		character.alphabet();
+		character.alphabetUpper();
+	}
+
+}
