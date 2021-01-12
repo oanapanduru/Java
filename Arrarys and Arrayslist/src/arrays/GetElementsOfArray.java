c96fde9f8221035ffc167a95379d3865b7584be7
oanap <oana.panduru@gmail.com>
2021-01-12 07:23:02 +0200

    example Array and ArrayList

---
+++ Arrarys and Arrayslist/src/arrays/GetElementsOfArray.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class GetElementsOfArray {
+
+	public static void main(String[] args) {
+		String[] var = {"ala", "bala", "portocala"};
+		for (String element : var) {
+			System.out.println(element);
+		}
+		//using for-each;
+		for (String i : var) {
+			System.out.println(i);
+		}
+
+	}
+
+}
