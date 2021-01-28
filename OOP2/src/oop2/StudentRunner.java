7d53a460e02656efe15d92ba5c2cfbd55edb5dfc
oanap <oana.panduru@gmail.com>
2021-01-28 21:33:03 +0200

    oop exercises

--- OOP2/src/oop2/StudentRunner.java
+++ OOP2/src/oop2/StudentRunner.java
@@ -6,7 +6,15 @@ public class StudentRunner {
 		Student student = new Student();
 		student.setName("Olive");
 		student.setEmail("blabla");
+		student.setPhoneNumber("637947538942");
+		student.setYear(3);
+		student.setCollege("University of X");
 
+		System.out.println("name " + student.getName());
+		System.out.println("email: " + student.getEmail());
+		System.out.println("hone number: " + student.getPhoneNumber());
+		System.out.println("year: " + student.getYear());
+		System.out.println("college: " + student.getCollege());
 	}
 
 }
