1eee19dca3d52bbff20ac76624919f128fcafb77
oanap <oana.panduru@gmail.com>
2021-01-28 08:06:14 +0200

    oop examples

---
+++ OOP2/src/oop2/StudentRunner.java
@@ -0,0 +1,12 @@
+package oop2;
+
+public class StudentRunner {
+
+	public static void main(String[] args) {
+		Student student = new Student();
+		student.setName("Olive");
+		student.setEmail("blabla");
+
+	}
+
+}
