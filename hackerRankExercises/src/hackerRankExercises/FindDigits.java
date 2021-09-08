a2cc6a0c0fd01ede9f8f1a124e6eb209d7ff1d19
Oana Panduru <oana.panduru@gmail.com>
2021-09-08 21:45:02 +0300

    hacker rank exercises

--- hackerRankExercises/src/hackerRankExercises/FindDigits.java
+++ hackerRankExercises/src/hackerRankExercises/FindDigits.java
@@ -21,10 +21,26 @@ public class FindDigits {
 		}
 		return count;
 	}
+	public static int findDigits2(int n) {
+		// Write your code here
+		int count = 0;
+		int digit = 0;
+		while(n>0) {
+			//int digit = number%10;
+			digit = n%10;
+			if(digit!=0 ) {
+				if(n%digit==0) {
+					count++;
+				}
+			}
+			n  = n/10;
+		}
+		return count;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(findDigits(124));
+		System.out.println(findDigits2(124));
 	}
 
 }
